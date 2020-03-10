840c9fa67344552f56e102e96c1ffd6f1886cb62
Carl Factora <IvantheTricourne@users.noreply.github.com>
2020-03-09 20:24:22 -0400

    Update launchDolphin.js

--- launchDolphin.js
+++ launchDolphin.js
@@ -1,10 +1,16 @@
-const { exec, execSync, execFile, spawn } = require("child_process");
+const { exec, execSync } = require("child_process");
 const path = require('path');
 const ora = require("ora");
+
+
+
 // determine os
 var isWin = process.platform === "win32";
 // sleep async
 const sleep = (s) => new Promise(resolve => setTimeout(resolve, s * 1000));
+
+
+
 // from config file
 const config = require(process.argv[2]);
 // set dolphin path
@@ -16,9 +22,25 @@ const replayObject = require(pathToReplayFile);
 const pathToMeleeIso = config["ISO"];
 // commands
 const dolphinExtension = isWin ? '.exe' : '';
-const dolphinCmd = `Dolphin${dolphinExtension} -i "${pathToReplayFile}" -e ${pathToMeleeIso}`;
+const dolphinCmd = `Dolphin${dolphinExtension} -i "${pathToReplayFile}" -e "${pathToMeleeIso}"`;
+
+
+
+// convert seconds to HH:mm:ss,ms
+// from: https://gist.github.com/vankasteelj/74ab7793133f4b257ea3
+function sec2time(timeInSeconds) {
+    var pad = function(num, size) { return ('000' + num).slice(size * -1); },
+        time = parseFloat(timeInSeconds).toFixed(3),
+        hours = Math.floor(time / 60 / 60),
+        minutes = Math.floor(time / 60) % 60,
+        seconds = Math.floor(time - minutes * 60),
+        milliseconds = time.slice(-3);
+    return pad(hours, 2) + ':' + pad(minutes, 2) + ':' + pad(seconds, 2)
+
+
+
 // exec
-async function debugExec(cmd) {
+async function launchDolphin(cmd) {
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
             console.log(`error: ${error.message}`);
@@ -30,12 +52,19 @@ async function debugExec(cmd) {
         }
         console.log(`stdout: ${stdout}`);
     });
-    console.log(`${path.basename(pathToReplayFile)} is being played...`);
-    const spinner = ora(`Wait ${replayObject.totalLengthSeconds} seconds for a special message!`).start();
+    const playing = ora(`${path.basename(pathToReplayFile)} is playing (length: ${sec2time(replayObject.totalLengthSeconds)}) ...`).start();
     await sleep(replayObject.totalLengthSeconds);
-    spinner.succeed();
-    console.log('Happy Leif Erikson day! HINGA DINGA DOERGEN!');
+    playing.succeed("Playback complete.");
+    // kill dolphin
+    if (isWin) {
+        execSync('taskkill /F /IM "Dolphin.exe" /T');
+    } else {
+        execSync('killall Dolphin');
+    }
 }
+
+
+
 // run
 process.chdir(pathToDolphin);
-debugExec(dolphinCmd);
+launchDolphin(dolphinCmd);
